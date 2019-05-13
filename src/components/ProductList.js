3ecf3e1aaf842575059317f87eac10fbe8201578
Anastasia Yanvareva <anastasiayanvareva@MacBook-Pro-2.local>
2019-05-13 11:23:17 +0200

    Fetch API

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,24 +1,63 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component, Fragment } from "react";
 
 class ProductList extends Component {
-    state={
-        products:[
-            {id:1, name:"Product 1", price: 2322},
-            {id:2, name:"Product 2", price: 10092},
-            {id:3, name:"Product 3", price: 32200}
-        ]
-    }
+  state = {
+    products: [],
+    selectedProduct: ""
+  };
+  handleOnClick = id => {
+    const selectedProduct = this.state.products.find(item => item.id === id);
+    // console.log(selectedProduct);
+    this.setState({
+      selectedProduct
+    });
+  };
+
+  async componentDidMount() {
+    let products = [];
+    const response = await fetch(
+      `https://dev-api.danielwellington.com/frontend/products`
+    );
+    const json = await response.json();
+    const api = json.data.map(async item => {
+      const productResponse = await fetch(
+        `https://dev-api.danielwellington.com/frontend/products/${item.id}`
+      );
+      const singleProduct = await productResponse.json();
+      let product = { id: singleProduct.data.id };
+
+      singleProduct.data.elements.map(item => {
+        if (item.name === "name") {
+          product.name = item.value;
+        }
+        if (item.name === "price") {
+          product.price = item.value.value;
+        }
+      });
+
+      products.push({...product});
+      this.setState({
+        products
+      });
+    });
+    console.log(api);
+  }
+
   render() {
     return (
       <Fragment>
-        <ul>
-            {
-                this.state.products.map(prod => <li key={prod.id}>{prod.name}: {prod.price}</li>)
-            }
-        </ul>
+        <div>
+          {this.state.products.map(prod => (
+            <div key={prod.id} onClick={() => this.handleOnClick(prod.id)}>
+              {prod.name}: {prod.price}
+            </div>
+          ))}
+        </div>
+
+        <div>{this.state.selectedProduct.name}</div>
       </Fragment>
-    )
+    );
   }
 }
 
-export default ProductList
+export default ProductList;
