13d3b4d7d13c864d2922952521d7eff1f9a16929
Anastasia Yanvareva <anastasiayanvareva@MacBook-Pro-2.local>
2019-05-14 23:27:45 +0200

    Footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ProductList from "./components/ProductList"
-import Header from "./common/Header"
+import React from "react";
+import ProductList from "./components/ProductList";
+import Header from "./common/Header";
+import Footer from "./common/Footer";
 
 function App() {
   return (
     <div>
-      <Header/>
-     <ProductList/>
+      <Header />
+      <ProductList />
+      <Footer/>
     </div>
   );
 }
---
+++ src/common/Footer.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+const FooterContainer = styled.div`
+  border-top: 1px solid #80808026;
+  width: 100%;
+  height: 5rem;
+  background-color: white;
+  bottom: 0;
+  p{
+    text-align: center;
+  }
+`;
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <p>&#169; 2019 Daniel Wellington AB</p>
+    </FooterContainer>
+  );
+};
+
+export default Footer;
